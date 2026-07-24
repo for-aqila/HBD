3843d2a589330a7b54f80e2079a64b95abe79eed
for-aqila <akunaku73580@gmail.com>
2026-07-25 06:16:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -132,7 +132,6 @@ function goNextPage() {
     if (currentLocation == paperTotal - 1) closeBook(0);
     else openBook();
 
-    // console.log(`Active now : ${currentLocation}`);
     currentLocation++;
   }
 }
@@ -196,13 +195,13 @@ function createEmoji() {
   element.classList.add("love-item");
   element.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"/></svg>`;
 
-  const size = Math.random() * 20 + 20; // Ukuran antara 20px dan 50px
+  const size = Math.random() * 20 + 20;
   element.style.width = `${size}px`;
 
   const leftPosition = Math.random() * window.innerWidth;
   element.style.left = `${leftPosition}px`;
 
-  const duration = Math.random() * 3 + 2; // Durasi antara 2 dan 5 detik
+  const duration = Math.random() * 3 + 2;
   element.style.animationDuration = `${duration}s`;
 
   document.querySelector(".bg-love").appendChild(element);
@@ -212,7 +211,6 @@ function createEmoji() {
   }, duration * 1000);
 }
 
-// setInterval(createEmoji, 500);
 
 window.onload = () => {
   const preload = document.querySelector(".preload");
